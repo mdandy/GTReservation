5d54c76194e829d9388e9345f314a1f04e47a868
Michael Dandy <mdandy1988@gmail.com>
2012-09-17 03:09:02 -0400

    Reservation Detail

--- app/scripts/ui.js
+++ app/scripts/ui.js
@@ -1,10 +1,50 @@
-function loadPage(page, isReversed)
+/**
+ * Go to the next page.
+ * @param {string} page The page to be loaded
+ * @param {string} func [Optional] Any function name to be executed after the page is loaded
+ * @param {string} arg [Optional] Any function argument to be passed
+ */
+function nextPage(page, func, arg)
 {
-	if (isReversed === undefined)
-		isReversed = false;
-		
 	$.mobile.changePage("#" + page, {
 		transition: "slide",
-		reverse: isReversed,
+		reverse: false,
 	});
+	
+	if(func)
+	{
+		if (arg === undefined)
+			arg = "";
+		eval(func + "('" + arg + "')");
+	}
+}
+
+/**
+ * Go to the previous page.
+ * @param {string} page The page to be loaded
+ * @param {string} func [Optional] Any function name to be executed after the page is loaded
+ * @param {string} arg [Optional] Any function argument to be passed
+ */
+function prevPage(page, func, arg)
+{	
+	$.mobile.changePage("#" + page, {
+		transition: "slide",
+		reverse: true,
+	});
+	
+	if(func)
+	{
+		if (arg === undefined)
+			arg = "";
+		eval(func + "('" + arg + "')");
+	}
+}
+
+/**
+ * Load reservation detail.
+ * @param {string} reservationID The reservation ID
+ */
+function loadReservationDetail(reservationID)
+{
+	$("#reservation_detail.page_content").html("<p>Load Reservation Detail: " + reservationID + "</p>");	
 }
