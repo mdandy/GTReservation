1adad01b68b5b438102618d1c60ac5d815a1a234
Michael Dandy <mdandy1988@gmail.com>
2012-09-19 16:03:08 -0400

    RPC-fied API

--- app/server/reservation.php
+++ app/server/reservation.php
@@ -1,53 +1,75 @@
 <?php
 
-function create_reservation()
+/**
+ * Create a reservation for CRC court. The interval of reservation is one hour and
+ * the user can only reserve up to 48 hours in advance.
+ * @param $user_id The user ID
+ * @param $court_number The court number to be reserved
+ * @param $start_time The reservation time in YYYY-MM-DD HH:MM:SS UTC format
+ * @return The court reservation ID
+ */
+function create_reservation($user_id, $court_number, $start_time)
 {
-	$user_id = $_POST["user_id"];
-	$court_number = $_POST["court_number"];
-	$start_time = $_POST["start_time"];
-	
-	$reservation_id = 0;
-	echo ($reservation_id);
+	$reservation_id = -1;
+	return $reservation_id;
 }
 
-function cancel_reservation_by_id()
+/**
+ * Cancel court reservation by reservation ID.
+ * @param $reservation_id The court reservation ID to be canceled
+ * @return TRUE on sucess or FALSE otherwise
+ */
+function cancel_reservation_by_id($reservation_id)
 {
-	$reservation_id = $_POST["reservation_id"];
-	
-	echo ("false");
+	return "FALSE";
 }
 
-function cancel_reservations_by_time()
+/**
+ * Cancel court reservation by reservation time.
+ * @param $court_number The court number to be reserved
+ * @param $start_time The reservation time in YYYY-MM-DD HH:MM:SS UTC format
+ * @param $interval [Optional] The time interval (in an increment of one hour). THe default value is one hour.
+ * @return TRUE on sucess or FALSE otherwise
+ */
+function cancel_reservations_by_time($court_number, $start_time, $interval)
 {
-	$court_number = $_POST["court_number"];
-	$start_time = $_POST["start_time"];
-	$interval = $_POST["interval"];
-	
-	echo ("false");
+	return "FALSE";
 }
 
-function get_all_reservations()
+/**
+ * Retrieve all reservations that have been made.
+ * @param $court_number The court number to be reserved or -1 to retrieve all court reservations
+ * @param $start_time The reservation starting time in YYYY-MM-DD HH:MM:SS UTC format
+ * @return The reservation information in JSON format
+ */
+function get_all_reservations($court_number, $start_time)
 {
-	$court_number = $_POST["court_number"];
-	$start_time = $_POST["start_time"];
-	
-	echo ("Not implemented");
+	$result = array("reservation_id" => "-1");
+	echo json_encode($result);
 }
 
-function get_reservations_by_user_id()
+/**
+ * Retrieve all reservations that have been made by a particular user.
+ * @param $user_id The user ID
+ * @return The reservation information in JSON format
+ */
+function get_reservations_by_user_id($user_id)
 {
-	$user_id = $_POST["user_id"];
-	
-	echo ("Not implemented");
+	$result = array("reservation_id" => "-1");
+	return json_encode($result);
 }
 
-function get_reservations_by_time()
+/**
+ * Retrieve all reservations that have been made at a particular time frame.
+ * @param $court_number The court number to be reserved or -1 to retrieve all court reservations
+ * @param $start_time The reservation starting time in YYYY-MM-DD HH:MM:SS UTC format
+ * @param $interval The time interval (in an increment of one hour)
+ * @return The reservation information in JSON format
+ */
+function get_reservations_by_time($court_number, $start_time, $interval)
 {
-	$court_number = $_POST["court_number"];
-	$start_time = $_POST["start_time"];
-	$interval = $_POST["interval"];
-	
-	echo ("Not implemented");
+	$result = array("reservation_id" => "-1");
+	return json_encode($result);
 }
 
 ?>
