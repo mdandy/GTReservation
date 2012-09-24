de68bc32ea0298858c80cf3f3740e4405ca213fa
Kevin <NoQuarterKJ@gmail.com>
2012-09-24 14:17:54 -0400

    proof of concept where you can login to facebook and post to wall

--- app/scripts/facebook.js
+++ app/scripts/facebook.js
@@ -1,4 +1,4 @@
-  window.fbAsyncInit = function() {
+window.fbAsyncInit = function() {
     FB.init({
       appId      : '414447185270616', // App ID
       channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
@@ -13,14 +13,37 @@
 function handleStatusChange(response) {
      document.body.className = response.authResponse ? 'connected' : 'not_connected';
 
-     if (response.authResponse) {
+     if (response.authResponse) { // user is logged in
        console.log(response);
+       //updateUserInfo(response);
+       //publishStory();
      }
    }
 
 function loginUser() {    
      FB.login(function(response) { }, {scope:'email'});     
      }
+
+function updateUserInfo(response) {
+     FB.api('/me', function(response) {
+       document.getElementById('user-info').innerHTML = '<img src="https://graph.facebook.com/' + response.id + '/picture">' + response.name;
+     });
+   }
+
+function publishStory() {
+  FB.ui({
+    method: 'feed',
+    name: 'I\'m building a social mobile web app!',
+    caption: 'This web app is going to be awesome.',
+    description: 'Check out Facebook\'s developer site to start building.',
+    link: 'https://developers.facebook.com/mobile',
+    picture: 'http://www.facebookmobileweb.com/getting-started/img/facebook_icon_large.png'
+  }, 
+  function(response) {
+    console.log('publishStory response: ', response);
+  });
+  return false;
+}
 
   // Load the SDK Asynchronously
   (function(d){
