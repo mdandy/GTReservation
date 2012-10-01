59d71c401434b837a59ede53118757552dfa356a
Michael Dandy <mdandy1988@gmail.com>
2012-10-01 18:12:43 -0400

    Add user API

---
+++ app/server/user.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once("dal.php");
+
+function add_user($user_id, $name, $email="")
+{
+	DAL::connect();
+	$isSuccessful  = DAL::insert_user($user_id, $name, $email);
+	DAL::disconnect();
+	
+	if ($isSuccessful)
+		return "TRUE";
+	return "FALSE";
+}
+
+function get_user($user_id)
+{
+	DAL::connect();
+	$data  = DAL::get_users($user_id);
+	DAL::disconnect();
+	return $data;
+}
+
+function delete_user($user_id)
+{
+	DAL::connect();
+	$isSuccessful  = DAL::delete_users($user_id);
+	DAL::disconnect();
+	
+	if ($isSuccessful)
+		return "TRUE";
+	return "FALSE";
+}
+
+?>
