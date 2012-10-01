d07733cd69ea55c33f2e83ed636a8413ccfefe5f
Kevin <NoQuarterKJ@gmail.com>
2012-10-01 16:41:11 -0400

    added date functionality as well as preliminary table generation

--- app/scripts/dateFunctions.js
+++ app/scripts/dateFunctions.js
@@ -1,3 +1,6 @@
+var today;
+var currDate;
+
 function leadingZero (val) {
             var str = val.toString();
             if(str.length == 1)
@@ -9,17 +12,38 @@ function leadingZero (val) {
 }
 
 function todaysDate() {
-    return this.futureDateDays(0);
+	currDate = Date.today().toString("ddd MMM d yyyy");
+	document.getElementById('dateRes').innerHTML = currDate;
+	today = currDate; // this is only called once so set todays date
+	$('#prevDate').addClass('ui-disabled'); // disable prev button
+	return currDate;
 }
 
-function tomorrowsDate(){
-    return this.futureDateDays(1);
+function tomorrowsDate(){	
+	Date.today().add(1).days().toString("ddd MMM d yyyy");
 }
 
-function futureDateDays(days) {
-    var futureDate = new Date();
-    futureDate.setDate(futureDate.getDate() + days);
-    var output = this.leadingZero(futureDate.getDate()) +'/'+ this.leadingZero(futureDate.getMonth() + 1) +'/'+ this.leadingZero(futureDate .getFullYear());
-    document.getElementById('dateRes').innerHTML = output; 
-    return this.leadingZero(futureDate.getDate()) +'/'+ this.leadingZero(futureDate.getMonth() + 1) +'/'+ this.leadingZero(futureDate .getFullYear());
+// Will return the previous date of the date the user was currently looking at
+function prevDate(){
+	$('#nextDate').removeClass('ui-disabled');
+	if (Date.parse(currDate).isAfter(Date.today())) { // only allow if the prevDate is post today
+		currDate = document.getElementById('dateRes').innerHTML;
+		currDate = Date.parse(currDate.toString("ddd MMM d yyyy")).add(-1).days().toString("ddd MMM d yyyy");
+		document.getElementById('dateRes').innerHTML = currDate;
+	}
+	if (!Date.parse(currDate).isAfter(Date.today())) { // check if updated date is today or earlier
+		$('#prevDate').addClass('ui-disabled');
+	}
+}
+
+function nextDate(){
+	$('#prevDate').removeClass('ui-disabled');
+	if (Date.parse(currDate).isBefore(Date.today().add(2).days())) {
+		currDate = document.getElementById('dateRes').innerHTML;
+		currDate = Date.parse(currDate.toString("ddd MMM d yyyy")).add(1).days().toString("ddd MMM d yyyy");
+		document.getElementById('dateRes').innerHTML = currDate;
+	} 
+	if (!Date.parse(currDate).isBefore(Date.today().add(2).days())) {
+		$('#nextDate').addClass('ui-disabled');
+	}
 }
--- app/scripts/ui.js
+++ app/scripts/ui.js
@@ -48,3 +48,55 @@ function loadReservationDetail(reservationID)
 {
 	$("#reservation_detail.page_content").html("<p>Load Reservation Detail: " + reservationID + "</p>");	
 }
+
+function generateTable(time, isReserved, isAvailable, reservation_id) {
+	//Date.parse(Date.today()).getDay();
+	var day = Date.parse(document.getElementById('dateRes').innerHTML).getDay();
+	var count;
+	if (day > 5) { // It is a Saturday or Sunday
+		count = 14;
+	}
+	else {
+		count = 16;
+	}
+	var template = "";
+	var i;
+	var times = new Array();
+	//TODO: Change to stop at 8 pm on Saturday-Sunday
+	times[0] = "6:00 AM";
+	times[1] = "7:00 AM";
+	times[2] = "8:00 AM";
+	times[3] = "9:00 AM";
+	times[4] = "10:00 AM";
+	times[5] = "11:00 AM";
+	times[6] = "12:00 PM";
+	times[7] = "1:00 PM";
+	times[8] = "2:00 PM";
+	times[9] = "3:00 PM";
+	times[10] = "4:00 PM";
+	times[11] = "5:00 PM";
+	times[12] = "6:00 PM";
+	times[13] = "7:00 PM";
+	times[14] = "8:00 PM";
+	times[15] = "9:00 PM";
+	times[16] = "10:00 PM";
+	template += "<div id='grid_header'>";
+	template += "<div class='ui-grid-d'>";
+	template += "<div class='grid_cell_header ui-block-a'></div>";
+	template += "<div class='grid_cell_header ui-block-b'>Court 1</div>";
+	template += "<div class='grid_cell_header ui-block-c'>Court 2</div>";
+	template += "<div class='grid_cell_header ui-block-d'>Court 3</div>";
+	template += "<div class='grid_cell_header ui-block-e'>Court 4</div>";
+	template += "</div>";
+	template += "</div>";
+	for (i = 0; i <= count; i++) {
+		template += "<div class='ui-grid-d'>";
+		template += "<div class='grid_cell ui-block-a'>" + times[i] + "</div>";
+		template += "<div class='grid_cell ui-block-b'></div>";
+		template += "<div class='grid_cell ui-block-c'></div>";
+		template += "<div class='grid_cell ui-block-d'></div>";
+		template += "<div class='grid_cell ui-block-e'></div>";
+		template += "</div>";
+	}
+	$("#create_reservations .page_content #container #grid_header").html(template);
+}
